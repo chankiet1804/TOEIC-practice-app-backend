60c02c3058774b170e2a1f84dbacb4afa71a265a
chankiet1804 <22520717@gm.uit.edu.vn>
2025-02-26 00:36:46 +0700

    create api login and using jwt

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const { name } = require("ejs");
+const e = require("express");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 
 const createUserService = async (name,email,password) => {
@@ -21,7 +25,52 @@ const createUserService = async (name,email,password) => {
     }
 }
 
+const loginService = async (email1,password) => {
+    try {
+        //fetch user by email from db
+        const user = await User.findOne({email: email1});
+        if(user) {
+            const isMatchPassword = await bcrypt.compare(password, user.password);
+            if(!isMatchPassword) {
+                return {
+                    EC: 2,
+                    EM: "Email/Password không hợp lệ",
+                }
+            }else {
+                const payload = {
+                    name: user.name,
+                    email: user.email,
+                }
+                const access_token = jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: process.env.JWT_EXPIRE
+                    }
+                );
+                return {
+                    EC: 0,
+                    access_token,
+                    user: {
+                        email: user.email,
+                        name: user.name,
+                    }
+                };
+            }
+        }else {
+            return {
+                EC: 1,
+                EM: "Email/Password không hợp lệ",
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
 
 module.exports = {
-    createUserService
+    createUserService,loginService
 }
